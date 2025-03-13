3882a500f29cd85adae9f72ee53a1786629fb8ac
Ali Çömez | Slaweally <sys@rootali.net>
2025-03-13 23:12:22 +0300

    Update turnstile.login.tags.php

--- turnstile.login.tags.php
+++ turnstile.login.tags.php
@@ -2,7 +2,7 @@
 /* ====================
 [BEGIN_COT_EXT]
 Hooks=users.auth.tags
-Tags=users.auth.tpl:{USERS_AUTH_VERIFYIMG};users.auth.tpl:{USERS_AUTH_VERIFYINPUT}
+Tags=users.auth.tpl:{USERS_AUTH_VERIFY_IMG};users.auth.tpl:{USERS_AUTH_VERIFY_INPUT}
 Order=10
 [END_COT_EXT]
 ==================== */
@@ -14,7 +14,7 @@ global $cfg, $usr, $t;
 if ($cfg['plugin']['turnstile']['enable_login'] && $usr['id'] == 0)
 {
     $t->assign([
-        'USERS_AUTH_VERIFYIMG' => cot_captcha_generate('turnstile'),
-        'USERS_AUTH_VERIFYINPUT' => ''
+        'USERS_AUTH_VERIFY_IMG'   => cot_captcha_generate('turnstile'),
+        'USERS_AUTH_VERIFY_INPUT' => ''
     ]);
 }
